0ab597cf1040c1dc0d0e06af247788201738a682
Ana María Timsa <anca.timsa@gmail.com>
2017-11-02 13:20:58 +0100

    Cambios estructura html y js carrusel

--- carrusel/js/carrusel.js
+++ carrusel/js/carrusel.js
@@ -1,52 +1,24 @@
 
 
 
-$( document ).ready(function() {
-
-	$("#slide1").click(function(){
-		$("#img1").animate({"margin-left":"-1125px"},1000);
-		console.log("imagen 1")
-		$("#img2").animate({"margin-left":"0"},1000);
-		
-
-	});
-    console.log( "ready1" );
-
+$('.carosel-control-right').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
+});
+$('.carosel-control-left').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
 });
 
 
-$("#slide2").click(function(){
-	$("#img2").animate({"margin-left":"0px"},1000);
-	$("#img3").animate({"margin-left":"1125px"},1000);
-	$("#img1").animate({"margin-left":"-1125px"},1000);
-	 console.log( "imagen 2 " );
+
+$('.bootom-rdio').click(function() {
+  $(this).blur();
+  console.log("entro aki")
+  $(".slide").parent().find(".carosel-item").first().insertAfter($(this).parent().find('.carosel-item').last());
+  console.log( this + " entro aki")
 });
-
-
-$("#slide4").click(function(){
-	$("#img4").animate({"margin-left":"-1125px"},1000);
-	 console.log( "imagen 4 " );
+$('.bootom-rdio').click(function() {
+  $(this).blur();
+  $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
 });
-
-// function definida en html en el boton que quieres clicar
-function imgClick3(){
-	$("#img3").animate({"margin-left":"-1125px"},1000);
-	// $("#img4").removeClass("imagen").addClass("imagenPrev");
-	console.log( "imagen 3 " );
-
-
-};
-
-
-$(document).on('mouseenter','#slide4',function () {
-    "use strict";
-    $(this).find('.img4').stop().animate({ 'opacity': '0' }, 800);
-    console.log("entro aki")
-});
-
-$(document).on('mouseleave','#slide4',function () {
-    "use strict";
-    $(this).find('.img4').stop().animate({ 'opacity': '1' }, 800);
-    console.log("entro aki")
-});
-  
