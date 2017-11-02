b38f1fab5930835ae7e21da01bd36df7c1949bfa
Ana María Timsa <anca.timsa@gmail.com>
2017-11-02 13:49:34 +0100

    cambios mouseenter

--- CVHTML/js/cv.js
+++ CVHTML/js/cv.js
@@ -17,266 +17,244 @@ $(document).ready(function(){
 				console.log( contenido2 + " :scrollTop :contenido2");
 				console.log( top + " :scrollTop :top");
 
-		               });
+		    });
+ 
+
+         
+			
+	    $( ".html" )
+			  .mouseover(function() {
+			    $( ".cicloHtml" ).animate({"background-position": "0px"},100 );
+			     $( this ).css({"padding-left": "10px"},100 );
+
+			     console.log("html")
+
+			  })
+			  .mouseout(function() {
+			   $( ".cicloHtml" ).animate({"background-position": "-65px"},100);
+			   $( ".paddingHover" ).css({"padding-left": "0px"},100 );
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
 
-
-			//    	$( ".html" ).mouseenter(function() {
-			// 		 // $(this).animate({"padding-right":"15px"},100);
-			// 	     $(".ciclo").animate({"background-size":"115px"},100);
-			// 	     console.log("background-size")
-			// });
-
-			//    	$( ".html" ).mouseleave(function() {
-			// 		 // $(this).animate({"padding-right":"0px"}, 100);
-			// 	     $(".ciclo").animate({"background-size":"-115px"},100);
-			// 	     console.log("background-size")
-			// });
-             
-					var i =0;
-			    $( ".html" )
-					  .mouseover(function() {
-					    $( ".cicloHtml" ).animate({"background-position": "0px"},100 );
-					     $( this ).css({"padding-left": "10px"},100 );
-
-					  })
-					  .mouseout(function() {
-					   $( ".cicloHtml" ).animate({"background-position": "-65px"},100);
-					   $( ".paddingHover" ).css({"padding-left": "0px"},100 );
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		 $( ".ccs" )
+			  .mouseover(function() {
+			    $( ".cicloCCS" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloCCS" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
 
-				 $( ".ccs" )
-					  .mouseover(function() {
-					    $( ".cicloCCS" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloCCS" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		 $( ".js" )
+			  .mouseover(function() {
+			    $( ".cicloJs" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloJs" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
+	     $( ".boot" )
+			  .mouseover(function() {
+			    $( ".cicloBoot" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloBoot" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				 $( ".js" )
-					  .mouseover(function() {
-					    $( ".cicloJs" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloJs" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".jQ" )
+			  .mouseover(function() {
+			    $( ".ciclojQuery" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".ciclojQuery" ).animate({"background-position": "-65px"},100);
+			  });
 
-			     $( ".boot" )
-					  .mouseover(function() {
-					    $( ".cicloBoot" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloBoot" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".subLine" )
+			  .mouseover(function() {
+			    $( ".ciclosubLine" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".ciclosubLine" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".jQ" )
-					  .mouseover(function() {
-					    $( ".ciclojQuery" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".ciclojQuery" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".netbeans" )
+			  .mouseover(function() {
+			    $( ".cicloNetbeans" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloNetbeans" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".subLine" )
-					  .mouseover(function() {
-					    $( ".ciclosubLine" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".ciclosubLine" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+	    $( ".dream" )
+			  .mouseover(function() {
+			    $( ".cicloDream" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloDream" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".netbeans" )
-					  .mouseover(function() {
-					    $( ".cicloNetbeans" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloNetbeans" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".dream" )
+			  .mouseover(function() {
+			    $( ".cicloDream" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloDream" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-			    $( ".dream" )
-					  .mouseover(function() {
-					    $( ".cicloDream" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloDream" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+            $( ".github" )
+			  .mouseover(function() {
+			    $( ".cicloGithub" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloGithub" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".dream" )
-					  .mouseover(function() {
-					    $( ".cicloDream" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloDream" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".sourceT" )
+			  .mouseover(function() {
+			    $( ".cicloSourceT" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloSourceT" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-                $( ".github" )
-					  .mouseover(function() {
-					    $( ".cicloGithub" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloGithub" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".Word" )
+			  .mouseover(function() {
+			    $( ".cicloWord" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloWord" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".sourceT" )
-					  .mouseover(function() {
-					    $( ".cicloSourceT" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloSourceT" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".excel" )
+			  .mouseover(function() {
+			    $( ".cicloExcel" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloExcel" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".Word" )
-					  .mouseover(function() {
-					    $( ".cicloWord" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloWord" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".point" )
+			  .mouseover(function() {
+			    $( ".cicloPoint" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloPoint" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".excel" )
-					  .mouseover(function() {
-					    $( ".cicloExcel" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloExcel" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+	    $( ".outlook" )
+			  .mouseover(function() {
+			    $( ".cicloOutlook" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloOutlook" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				$( ".point" )
-					  .mouseover(function() {
-					    $( ".cicloPoint" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloPoint" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+	    $( ".idEspañol" )
+			  .mouseover(function() {
+			    $( ".cicloIdEspañol" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloIdEspañol" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-			    $( ".outlook" )
-					  .mouseover(function() {
-					    $( ".cicloOutlook" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloOutlook" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		 $( ".idIngles" )
+			  .mouseover(function() {
+			    $( ".cicloidIngles" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloidIngles" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-			    $( ".idEspañol" )
-					  .mouseover(function() {
-					    $( ".cicloIdEspañol" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloIdEspañol" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
+		$( ".idRumano" )
+			  .mouseover(function() {
+			    $( ".cicloIdRumano" ).animate({"background-position": "0px"},100 );
+			  })
+			  .mouseout(function() {
+			   $( ".cicloIdRumano" ).animate({"background-position": "-65px"},100);
+			    // $( ".ciclo" ).text( ++i );
+			  });
 
-				 $( ".idIngles" )
-					  .mouseover(function() {
-					    $( ".cicloidIngles" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloidIngles" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
-
-				$( ".idRumano" )
-					  .mouseover(function() {
-					    $( ".cicloIdRumano" ).animate({"background-position": "0px"},100 );
-					  })
-					  .mouseout(function() {
-					   $( ".cicloIdRumano" ).animate({"background-position": "-65px"},100);
-					    // $( ".ciclo" ).text( ++i );
-					  });
-
-
-
-
-
-
-                });
-
-
+});
 			
 
+		// /* obtener valor */
+		// $("#section2-2").offset();
+		// /* establecer valor */
+		// $("#section2-2").offset({top:value,left:value});
+		// console.log( "left: " + position.left + ", top: " + position.top);
+
+
+		// var coordenadas = $("#section2-2").position();
+
+		// $(document).ready(function(){
+		//  	$("#section2").click(function(){
+		//  		var coordenadas = $("#section2-2").position();
+		// 		$("#section2-2").html("Y: " + coordenadas.top + " X: " + coordenadas.left);
+		// 		console.log("Y: " + coordenadas.top + " X: " + coordenadas.left);
+		// 	});   	
+		// });
+
+
+		// $(window).scroll(function(){
+		//     var coordenadas = $("#section2-2").position();
+		//      var scroll = $(window).scrollTop();
+		//      console.log(scrollTop);
+
+		//     if (coordenadas == -200 ){
+		//     	$(coordenadas).css({
+		//     		"left":"200px"
+		//     	});
+		//     	console.log("left")
+		//     }
+
+		// });
 
 
 
 
-// /* obtener valor */
-// $("#section2-2").offset();
-// /* establecer valor */
-// $("#section2-2").offset({top:value,left:value});
-// console.log( "left: " + position.left + ", top: " + position.top);
+		$(window).scroll( function() { 
+			 var scrolled_val = $(document).scrollTop().valueOf();
+			 console.log(scrolled_val+ ' = scroll value');
+
+			     if (scrolled_val >= 200) {
+			     		$("#section2-2").animate({"left":"0"});
+			     		$("#section2-2").fadeIn(100);
+
+		    	}else{
+		    			$("#section2-2").animate({"left":"-800px"});
+		    			$("#section2-2").fadeOut(100);
+		    	};
+		    	console.log("no va")
+
+		    	 if (scrolled_val >= 200) {
+			     		$("#section2-3").animate({"right":"0"});
+			     		$("#section2-3").fadeIn(100);
+
+		    	}else{
+		    			$("#section2-3").animate({"right":"-800px"});
+		    			$("#section2-3").fadeOut(100);
+		    	};
+		    	console.log("no va")
 
 
-// var coordenadas = $("#section2-2").position();
-
-// $(document).ready(function(){
-//  	$("#section2").click(function(){
-//  		var coordenadas = $("#section2-2").position();
-// 		$("#section2-2").html("Y: " + coordenadas.top + " X: " + coordenadas.left);
-// 		console.log("Y: " + coordenadas.top + " X: " + coordenadas.left);
-// 	});   	
-// });
-
-
-// $(window).scroll(function(){
-//     var coordenadas = $("#section2-2").position();
-//      var scroll = $(window).scrollTop();
-//      console.log(scrollTop);
-
-//     if (coordenadas == -200 ){
-//     	$(coordenadas).css({
-//     		"left":"200px"
-//     	});
-//     	console.log("left")
-//     }
-
-// });
-
-
-
-
-$(window).scroll( function() { 
-	 var scrolled_val = $(document).scrollTop().valueOf();
-	 console.log(scrolled_val+ ' = scroll value');
-
-	     if (scrolled_val >= 200) {
-	     		$("#section2-2").animate({"left":"0"});
-	     		$("#section2-2").fadeIn(100);
-
-    	}else{
-    			$("#section2-2").animate({"left":"-800px"});
-    			$("#section2-2").fadeOut(100);
-    	};
-    	console.log("no va")
-
-    	 if (scrolled_val >= 200) {
-	     		$("#section2-3").animate({"right":"0"});
-	     		$("#section2-3").fadeIn(100);
-
-    	}else{
-    			$("#section2-3").animate({"right":"-800px"});
-    			$("#section2-3").fadeOut(100);
-    	};
-    	console.log("no va")
-
-
-    
-    });
+		    
+		    });
 
  
 
--- carrusel/js/carrusel.js
+++ carrusel/js/carrusel.js
@@ -4,10 +4,12 @@
 $('.carosel-control-right').click(function() {
   $(this).blur();
   $(this).parent().find('.carosel-item').first().insertAfter($(this).parent().find('.carosel-item').last());
+  console.log("flecha derecha")
 });
 $('.carosel-control-left').click(function() {
   $(this).blur();
   $(this).parent().find('.carosel-item').last().insertBefore($(this).parent().find('.carosel-item').first());
+   console.log("flecha izquierda")
 });
 
 
